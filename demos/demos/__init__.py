88b6a6c97b1e0f44aabf5305ce90e7f490f5dc44
P. Christeas <xrg@linux.gr>
2015-10-20 15:54:12 +0300

    settings/celery: avoid having same module name as ext. lib
    Calling "import celery" from a module called "celery" itself may
    not work in Python2.

--- demos/demos/__init__.py
+++ demos/demos/__init__.py
@@ -1,4 +1,8 @@
 # File: __init__.py
 
-from .settings import demos
+from .settings import base
 
+if set(base.DEMOS_APPS).intersection(['ea', 'bds', 'abb']):
+	from .settings.celeryapp import app as celery_app
+
+#eof
