0115efa7b45d68f043ef1abb27d06ee2e193db60
Marios Levogiannis <lev.marios@gmail.com>
2015-10-21 01:44:31 +0300

    demos/urls.py: fix url patterns

--- demos/demos/urls.py
+++ demos/demos/urls.py
@@ -27,13 +27,9 @@ urlpatterns = []
 
 for iapp in settings.DEMOS_APPS:
     urlconfig = import_module('demos.apps.%s.urls' % iapp)
-    durl = settings.DEMOS_URL[iapp]
-    if durl:
-        durl = '^' + durl + '/'
-    else:
-        durl = '^'
-    urlpatterns += i18n_patterns(url(durl, include(urlconfig, namespace=iapp, app_name=iapp)),
-                    url(durl + 'api/', include(urlconfig.apipatterns, namespace='api', app_name=iapp))
-                    )
+    durl = '^' + (iapp if len(settings.DEMOS_APPS) > 1 else '') + '/'
+    urlpatterns += i18n_patterns(url(durl, include(urlconfig, namespace=iapp, app_name=iapp))) + \
+        [url(durl + 'api/', include(urlconfig.apipatterns, namespace='api', app_name=iapp))]
 
 #eof
+
