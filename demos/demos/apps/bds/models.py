654214cf9c6a0fdf30857a2fa1efb8e1c0f1f43b
Marios Levogiannis <lev.marios@gmail.com>
2015-10-21 03:07:38 +0300

    bds,vbb: remove protobuf dependency

--- demos/demos/apps/bds/models.py
+++ demos/demos/apps/bds/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.core import urlresolvers
 
-from demos.common.utils import config, crypto, enums, fields, storage
+from demos.common.utils import config, enums, fields, storage
 
 
 class Config(models.Model):
--- demos/demos/apps/vbb/models.py
+++ demos/demos/apps/vbb/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.core import urlresolvers
 
-from demos.common.utils import config, crypto, enums, fields
+from demos.common.utils import config, enums, fields
 
 
 class Config(models.Model):
@@ -156,7 +156,7 @@ class OptionV(models.Model):
 	long_votecode_hash = models.CharField(max_length=config.HASH_LEN,
 		blank=True, default='')
 	
-	receipt = models.CharField(max_length=config.RECEIPT_LEN)
+	receipt = models.CharField(max_length=config.HASH_LEN)
 	index = models.PositiveSmallIntegerField()
 	
 	# Other model methods and meta options
