7ebc4b9c364a92d79c475e119c2c80d6c60ff44e
Marios Levogiannis <lev.marios@gmail.com>
2015-10-22 17:47:31 +0300

    common/election_box.html: eliminate the need for custom templatetags

--- demos/demos/common/templatetags/custom.py
+++ demos/demos/common/templatetags/custom.py
@@ -5,18 +5,7 @@ from django.utils import timezone
 
 register = template.Library()
 
-@register.assignment_tag(name='now')
-def now_(*args):
-    return timezone.now()
-
 @register.assignment_tag(name='tuple')
 def tuple_(*args):
     return tuple(args)
-
-@register.filter(name='div')
-def div_(value, arg):
-    try:
-        return value / arg
-    except Exception:
-        return ''
 
