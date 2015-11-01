935441300e5f3fa4bd5167a01ffd92bcbab46929
P. Christeas <xrg@linux.gr>
2015-11-01 17:08:45 +0200

    bds/app: config check for TARSTORAGE_ROOT

--- demos/demos/apps/bds/apps.py
+++ demos/demos/apps/bds/apps.py
@@ -2,8 +2,34 @@
 
 from django.apps import AppConfig as _AppConfig
 from django.utils.translation import ugettext_lazy as _
-
+from django.core import checks as _checks
 
 class AppConfig(_AppConfig):
     name = 'demos.apps.bds'
     verbose_name = _('Ballot Distribution Center')
+
+
+
+@_checks.register(deploy=True)
+def tar_storage_check(app_configs, **kwargs):
+    """Tests basic socket connectivity with crypto service
+    """
+
+    from demos.common.utils import config
+    import tempfile
+    import os
+
+    try:
+        fd, path = tempfile.mkstemp(dir=config.TARSTORAGE_ROOT)
+        print "created tmp at:", path
+        os.close(fd)
+        os.unlink(path)
+        
+        return []
+    except Exception, e:
+        return [_checks.Error("Tar storage \"%s\" check failed: %s" % \
+                                (config.TARSTORAGE_ROOT, e),
+                              hint="Check that directory exists and is writable")
+                ]
+
+#eof
