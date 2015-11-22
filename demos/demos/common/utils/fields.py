29a78dbf76ad1b81918335f730031bb427bc9fd6
Marios Levogiannis <lev.marios@gmail.com>
2015-11-22 18:22:58 +0200

    merge ea/fields.py (form fields) with common/fields.py (model fields)

--- demos/demos/common/utils/fields.py
+++ demos/demos/common/utils/fields.py
@@ -2,14 +2,21 @@
 
 from __future__ import division, unicode_literals
 
+import re
+
 from base64 import b64encode, b64decode
 
+from django import forms
 from django.db import models
+from django.core import validators
 from django.utils import six
+from django.utils.dateparse import parse_datetime
 from django.core.exceptions import ValidationError
 
 from demos.common.utils import base32cf
 
+
+# Model fields -----------------------------------------------------------------
 
 class IntEnumField(models.SmallIntegerField):
     
@@ -172,4 +179,68 @@ class ProtoField(models.BinaryField):
     def value_to_string(self, obj):
         
         return b64encode(obj).decode('ascii')
+
+
+# Form fields -----------------------------------------------------------------
+
+class MultiEmailField(forms.Field):
+    
+    def __init__(self, min_length=None, max_length=None, *args, **kwargs):
+        
+        self.min_length, self.max_length = min_length, max_length * 254
+        super(MultiEmailField, self).__init__(*args, **kwargs)
+        
+        if min_length is not None:
+            min_validator = validators.MinLengthValidator(int(min_length))
+            self.validators.append(min_validator)
+        
+        if max_length is not None:
+            max_validator = validators.MaxLengthValidator(int(max_length))
+            self.validators.append(max_validator)
+    
+    def to_python(self, value):
+        """Normalize data to a list of strings."""
+        
+        if not value:
+            value_list = []
+        else:
+            value_list = re.split(r'[\s;,]+', value)
+            value_list = sorted(filter(None, set(value_list)))
+        
+        return value_list
+
+    def validate(self, value):
+        """Check if value consists only of valid emails."""
+        
+        super(MultiEmailField, self).validate(value)
+
+        for email in value:
+            validators.validate_email(email)
+
+    def widget_attrs(self, widget):
+        """Set the HTML attribute maxlength"""
+        
+        attrs = super(MultiEmailField, self).widget_attrs(widget)
+        
+        if self.max_length is not None:
+            attrs.update({'maxlength': str(self.max_length)})
+        
+        return attrs
+
+
+class DateTimeField(forms.DateTimeField):
+    """DateTimeField that supports the ISO8601 format"""
+    
+    def to_python(self, value):
+        
+        if isinstance(value, six.string_types):
+            try:
+                parsed_value = parse_datetime(value)
+            except ValueError:
+                pass
+            else:
+                if parsed_value is not None:
+                    value = parsed_value
+        
+        return super(DateTimeField, self).to_python(value)
 
