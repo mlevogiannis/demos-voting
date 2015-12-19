f6e6e2d67caddcc4b5e43ec6edc8c0b4be1b0b1f
Marios Levogiannis <lev.marios@gmail.com>
2015-12-19 15:22:50 +0200

    common/models/signals.py: rename pre_delete handler

--- demos/demos/common/conf/app_config.py
+++ demos/demos/common/conf/app_config.py
@@ -9,7 +9,7 @@ from django.db.models.signals import pre_delete
 from django.utils.functional import lazy
 
 from demos.common.conf import constants
-from demos.common.models.signals import pre_delete_protected_handler
+from demos.common.models.signals import pre_delete_protect_handler
 
 
 class AppConfig(_AppConfig):
@@ -24,8 +24,8 @@ class AppConfig(_AppConfig):
         # Prevent deletion of Election objects
         
         Election = self.get_model('Election')
-        pre_delete.connect(pre_delete_protected_handler, sender=Election,
-                           dispatch_uid='election_pre_delete_protected_handler')
+        pre_delete.connect(pre_delete_protect_handler, sender=Election,
+                           dispatch_uid='election_pre_delete_protect_handler')
         
         # Workaround for abstract natural key dependencies. For child classes
         # that do not override their parent class' natural_key method, any
--- demos/demos/common/models/signals.py
+++ demos/demos/common/models/signals.py
@@ -3,6 +3,6 @@
 from django.db import IntegrityError
 
 
-def pre_delete_protected_handler(sender, **kwargs):
+def pre_delete_protect_handler(sender, **kwargs):
     raise IntegrityError("Cannot delete instances of protected model '%s'" % sender.__name__)
 
