c84ad7206089cfa77e7bceb5f532a41a7f0f4b15
Marios Levogiannis <lev.marios@gmail.com>
2016-04-15 00:38:50 +0300

    common: fix abstract natural key dependency handling

--- demos/demos/common/conf/app_config.py
+++ demos/demos/common/conf/app_config.py
@@ -4,7 +4,6 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 
 from django.apps import AppConfig
 from django.db.models.signals import pre_delete
-from django.utils.functional import lazy
 
 from demos.common.signals import pre_delete_protect_handler
 
@@ -19,10 +18,8 @@ class AppConfig(AppConfig):
         pre_delete.connect(pre_delete_protect_handler, sender=Election,
                            dispatch_uid='election_pre_delete_protect_handler')
         
-        # Abstract natural key dependencies. If the 'app_label' part of a
-        # dependency is missing (app_label.model_label), the lower-cased
-        # name of the app they are contained within is added (it works like
-        # model relationships on models that have not been defined yet).
+        # Abstract natural key dependencies. Add the 'app_label' part of a
+        # dependency, if it is missing (app_label.model_name).
         
         for model in self.get_models():
             
@@ -31,18 +28,21 @@ class AppConfig(AppConfig):
                 dependencies = []
                 for dep in model.natural_key.dependencies:
                     if '.' not in dep:
-                        dep = '%s.%s' % (self.label.lower(), dep)
+                        dep = '%s.%s' % (self.label, dep)
                     dependencies.append(dep)
                 
                 if dependencies != model.natural_key.dependencies:
                     
-                    if 'natural_key' in vars(model):
-                        natural_key = model.natural_key
-                    else:
-                        # Add a proxy natural_key method if the class does
-                        # not define its own (i.e. inherits its parent's one)
-                        natural_key = lazy(model.natural_key, tuple)
-                        setattr(model, 'natural_key', natural_key)
+                    # Add a proxy natural_key method, if the class does not
+                    # define its own (i.e. inherits its parent's method)
                     
-                    natural_key.dependencies = dependencies
+                    if 'natural_key' not in vars(model):
+                        def natural_key(self, _model=model, *args, **kwargs):
+                            return super(_model, self).natural_key(*args, **kwargs)
+                        model.natural_key = natural_key
+                    
+                    try:
+                        model.natural_key.dependencies = dependencies
+                    except AttributeError:
+                        model.natural_key.__func__.dependencies = dependencies
 
