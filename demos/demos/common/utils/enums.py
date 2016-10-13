xrg/demos

models.py: replace Election model's 'long_votecodes' field
- long_votecodes used to be a boolean, vc_type in an integer
- improved code readability, checking whether short or long votecodes are used is now explicit

demos/demos/common/utils/enums.py
@@ -49,4 +49,9 @@ class State(IntEnumC):
     ERROR = 7
     TEMPLATE = 8
 
+class Vc(IntEnumC):
+    
+    SHORT = 1
+    LONG = 2
+
 #eof

mlevogiannis/demos-voting

demos/demos/common/utils/enums.py
@@ -24,3 +24,9 @@ class State(IntEnum):
     ERROR = 7
     TEMPLATE = 8
 
+@unique
+class Vc(IntEnum):
+    
+    SHORT = 1
+    LONG = 2
+