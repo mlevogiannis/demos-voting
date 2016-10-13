xrg/demos

commit b32448d6f9c6065361777e9c630c33accc04f4c7

utils/config: django-check for inter-server connectivity
./manage.py check --deploy
will now test connection between servers, crypto and report
if our setup is working.

demos/demos/common/utils/config.py
@@ -22,3 +22,63 @@ for iapp in settings.DEMOS_APPS:
     for key, value in settings.DEMOS_CONFIG[iapp].items():
         setattr(_config, key, value)
 
+
+from django.core import checks as _checks
+
+@_checks.register(deploy=True)
+def api_connectivity_check(app_configs, **kwargs):
+    from django.apps import apps
+    from demos.common.utils import api
+    # Dict of applications that connect to remote ones
+    connectivity_list = {'ea': ['bds', 'abb', 'vbb'], 'abb': ['ea'],
+                         'vbb': ['abb']
+                         }
+    messages = []
+    if app_configs is None:
+        app_configs = settings.DEMOS_APPS
+    
+    for local_app in app_configs:
+        app_config = apps.get_app_config(local_app)
+        ok_apps = []
+        for remote in connectivity_list.get(local_app, []):
+            try:
+                 api.Session(local_app, remote, app_config)
+                 ok_apps.append(remote)
+            except Exception, e:
+                messages.append(_checks.Error("Could not connect from %s to %s: %s" % \
+                                                (local_app, remote, e),
+                                              hint="Try running ./manage.py createusers_%s" % local_app))
+        if ok_apps:
+            messages.append(_checks.Info("Checking application %s: Login to %s OK" % \
+                                            (local_app, ', '.join(ok_apps)),
+                                        hint=None))
+
+    return messages
+
+@_checks.register(deploy=True)
+def crypto_connectivity_check(app_configs, **kwargs):
+    """Tests basic socket connectivity with crypto service
+    """
+    import socket
+    from demos.common.utils import crypto
+
+    try:
+        af = getattr(socket, _config.CRYPTO_AF)
+        sock = socket.socket(af)
+        
+        sock.settimeout(_config.RECV_TIMEOUT)
+        sock.connect(_config.CRYPTO_ADDR)
+        
+        sock.sendall('') # write something, will test +w flag on sockets
+        sock.shutdown(socket.SHUT_RDWR)
+        sock.close()
+        return [ _checks.Info("Checking connectivity with crypto: \"%s\" OK" % \
+                                _config.CRYPTO_ADDR) ]
+    except Exception, e:
+        return [_checks.Error("Connectivity with crypto \"%s\" failed: %s" % \
+                                ( _config.CRYPTO_ADDR, e),
+                              hint="Check that crypto service is running, properly configured")
+                ]
+
+
+# eof

mlevogiannis/demos-voting

commit 4af24415093a6ed9e06367c36d834c508ad87eac

utils/config.py: python3 compatibility changes
- fixes: b32448d6f9c6065361777e9c630c33accc04f4c7

demos/demos/common/utils/config.py
@@ -44,7 +44,7 @@ def api_connectivity_check(app_configs, **kwargs):
             try:
                  api.Session(local_app, remote, app_config)
                  ok_apps.append(remote)
-            except Exception, e:
+            except Exception as e:
                 messages.append(_checks.Error("Could not connect from %s to %s: %s" % \
                                                 (local_app, remote, e),
                                               hint="Try running ./manage.py createusers_%s" % local_app))
@@ -74,7 +74,7 @@ def crypto_connectivity_check(app_configs, **kwargs):
         sock.close()
         return [ _checks.Info("Checking connectivity with crypto: \"%s\" OK" % \
                                 _config.CRYPTO_ADDR) ]
-    except Exception, e:
+    except Exception as e:
         return [_checks.Error("Connectivity with crypto \"%s\" failed: %s" % \
                                 ( _config.CRYPTO_ADDR, e),
                               hint="Check that crypto service is running, properly configured")