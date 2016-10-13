xrg/demos

initial parties and candidates implementation
- currently only the ea's user interface is working

demos/demos/common/utils/enums.py
@@ -38,6 +38,9 @@ class IntEnumC:
 
         return ret
 
+
+# Enum definitions -------------------------------------------------------------
+
 class State(IntEnumC):
 
     DRAFT = 1
@@ -48,6 +51,11 @@ class State(IntEnumC):
     PAUSED = 6
     ERROR = 7
     TEMPLATE = 8
+
+class Type(IntEnumC):
+
+    ELECTIONS = 1
+    REFERENDUM = 2
 
 class VcType(IntEnumC):
     

mlevogiannis/demos-voting

demos/demos/common/utils/enums.py
@@ -12,6 +12,8 @@ class IntEnum(IntEnum):
         return str(int(self))
 
 
+# Enum definitions -------------------------------------------------------------
+
 @unique
 class State(IntEnum):
     
@@ -23,6 +25,12 @@ class State(IntEnum):
     PAUSED = 6
     ERROR = 7
     TEMPLATE = 8
+
+@unique
+class Type(IntEnum):
+
+    ELECTIONS = 1
+    REFERENDUM = 2
 
 @unique
 class VcType(IntEnum):