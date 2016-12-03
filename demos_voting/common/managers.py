b058d0e036050f7239398dca4c352666997c06be
Marios Levogiannis <marios.levogiannis@gmail.com>
2016-12-04 01:07:00 +0200

    Update managers' get_by_natural_key() methods

--- demos_voting/common/managers.py
+++ demos_voting/common/managers.py
@@ -8,8 +8,28 @@ from django.db import models
 
 class ElectionManager(models.Manager):
 
-    def get_by_natural_key(self, e_id):
-        return self.get(id=e_id)
+    def get_by_natural_key(self, election_id):
+        return self.get(id=election_id)
+
+
+class QuestionManager(models.Manager):
+
+    def get_by_natural_key(self, election_id, question_index):
+
+        election_manager = self.model._meta.get_field('election').related_model.objects.db_manager(self.db)
+        election = election_manager.get_by_natural_key(election_id)
+
+        return self.get(election=election, index=question_index)
+
+
+class OptionManager(models.Manager):
+
+    def get_by_natural_key(self, election_id, question_index, option_index):
+
+        question_manager = self.model._meta.get_field('question').related_model.objects.db_manager(self.db)
+        question = question_manager.get_by_natural_key(election_id, question_index)
+
+        return self.get(question=question, index=option_index)
 
 
 class BallotManager(models.Manager):
@@ -40,81 +60,53 @@ class BallotManager(models.Manager):
 
         return super(BallotManager, self).prefetch_related(*lookups)
 
-    def get_by_natural_key(self, e_id, b_serial_number):
+    def get_by_natural_key(self, election_id, ballot_serial_number):
 
-        model = self.model._meta.get_field('election').related_model
-        manager = model.objects.db_manager(self.db)
-        election = manager.get_by_natural_key(e_id)
+        election_manager = self.model._meta.get_field('election').related_model.objects.db_manager(self.db)
+        election = election_manager.get_by_natural_key(election_id)
 
-        return self.get(election=election, serial_number=b_serial_number)
+        return self.get(election=election, serial_number=ballot_serial_number)
 
 
 class PartManager(models.Manager):
 
-    def get_by_natural_key(self, e_id, b_serial_number, p_tag):
+    def get_by_natural_key(self, election_id, ballot_serial_number, part_tag):
 
-        model = self.model._meta.get_field('ballot').related_model
-        manager = model.objects.db_manager(self.db)
-        ballot = manager.get_by_natural_key(e_id, b_serial_number)
+        ballot_manager = self.model._meta.get_field('ballot').related_model.objects.db_manager(self.db)
+        ballot = ballot_manager.get_by_natural_key(election_id, ballot_serial_number)
 
-        return self.get(ballot=ballot, tag=p_tag)
-
-
-class QuestionManager(models.Manager):
-
-    def get_by_natural_key(self, e_id, q_index):
-
-        model = self.model._meta.get_field('election').related_model
-        manager = model.objects.db_manager(self.db)
-        election = manager.get_by_natural_key(e_id)
-
-        return self.get(election=election, index=q_index)
-
-
-class OptionManager(models.Manager):
-
-    def get_by_natural_key(self, e_id, q_index, o_index):
-
-        model = self.model._meta.get_field('question').related_model
-        manager = model.objects.db_manager(self.db)
-        question = manager.get_by_natural_key(e_id, q_index)
-
-        return self.get(question=question, index=o_index)
-
-
-class POptionManager(models.Manager):
-
-    def get_by_natural_key(self, e_id, b_serial_number, p_tag, q_index, o_index):
-
-        model = self.model._meta.get_field('question').related_model
-        manager = model.objects.db_manager(self.db)
-        question = manager.get_by_natural_key(e_id, b_serial_number, p_tag, q_index)
-
-        return self.get(question=question, index=o_index)
+        return self.get(ballot=ballot, tag=part_tag)
 
 
 class PQuestionManager(models.Manager):
 
-    def get_by_natural_key(self, e_id, b_serial_number, p_tag, q_index):
+    def get_by_natural_key(self, election_id, ballot_serial_number, part_tag, question_index):
 
-        model = self.model._meta.get_field('part').related_model
-        manager = model.objects.db_manager(self.db)
-        part = manager.get_by_natural_key(e_id, b_serial_number, p_tag)
+        part_manager = self.model._meta.get_field('part').related_model.objects.db_manager(self.db)
+        part = part_manager.get_by_natural_key(election_id, ballot_serial_number, part_tag)
 
-        model = self.model._meta.get_field('question').related_model
-        manager = model.objects.db_manager(self.db)
-        question = manager.get_by_natural_key(e_id, q_index)
+        question_manager = self.model._meta.get_field('question').related_model.objects.db_manager(self.db)
+        question = question_manager.get_by_natural_key(election_id, question_index)
 
         return self.get(part=part, question=question)
 
 
+class POptionManager(models.Manager):
+
+    def get_by_natural_key(self, election_id, ballot_serial_number, part_tag, question_index, option_index):
+
+        p_question_manager = self.model._meta.get_field('question').related_model.objects.db_manager(self.db)
+        p_question = p_question_manager.get_by_natural_key(election_id, ballot_serial_number, part_tag, question_index)
+
+        return self.get(question=p_question, index=option_index)
+
+
 class TaskManager(models.Manager):
 
-    def get_by_natural_key(self, e_id, task_id):
+    def get_by_natural_key(self, election_id, task_id):
 
-        model = self.model._meta.get_field('election').related_model
-        manager = model.objects.db_manager(self.db)
-        election = manager.get_by_natural_key(e_id)
+        election_manager = self.model._meta.get_field('election').related_model.objects.db_manager(self.db)
+        election = election_manager.get_by_natural_key(election_id)
 
         return self.get(election=election, task_id=task_id)
 
@@ -129,8 +121,7 @@ class PrivateApiNonceManager(models.Manager):
 
     def get_by_natural_key(self, app_label, nonce, timestamp, type):
 
-        model = self.model._meta.get_field('user').related_model
-        manager = model.objects.db_manager(self.db)
-        user = manager.get_by_natural_key(app_label)
+        user_manager = self.model._meta.get_field('user').related_model.objects.db_manager(self.db)
+        user = user_manager.get_by_natural_key(app_label)
 
         return self.get(user=user, nonce=nonce, timestamp=timestamp, type=type)
